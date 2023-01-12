8e4b5b282f7f3db946175f4313558669ed553d86
Ryan Wang <i@ryanc.cc>
2023-01-12 14:58:11 +0800

    feat: add create comment by custom account support (#2)
    支持通过自行填写账号信息来评论。
    
    目前的逻辑是：
    
    1. 如果有登录，则不显示 `昵称` `电子邮箱` `网站` 的输入框。
    2. 暂时没有判断是否开启了 `仅允许注册用户评论`。 https://github.com/halo-dev/halo/issues/3117
    
    <img width="909" alt="image" src="https://user-images.githubusercontent.com/21301288/211269817-81a87a64-79f0-4098-800c-80f8a7413a62.png">
    
    /kind improvement
    
    ```release-note
    支持通过自行填写账号信息来评论
    ```

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import Comment from "@/components/Comment.vue";
-import "@halo-dev/components/dist/style.css";
 import "@/styles/tailwind.css";
 import "@/styles/dark.css";
 import "@/styles/index.css";
+import "@halo-dev/components/dist/style.css";
 import type { App, Plugin } from "vue";
 
 const plugin: Plugin = {
