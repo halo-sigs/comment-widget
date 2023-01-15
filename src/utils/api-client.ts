b6074d21707fef64d66e1cb9427e67f32fd7e9d6
Ryan Wang <i@ryanc.cc>
2023-01-15 13:44:55 +0800

    perf: use alert api instead of toast component

--- src/utils/api-client.ts
+++ src/utils/api-client.ts
@@ -2,7 +2,6 @@ import {
   ApiConsoleHaloRunV1alpha1UserApi,
   ApiHaloRunV1alpha1CommentApi,
 } from "@halo-dev/api-client";
-import { Toast } from "@halo-dev/components";
 import axios, { AxiosError } from "axios";
 
 const apiUrl = import.meta.env.VITE_API_URL;
@@ -25,21 +24,21 @@ axiosInstance.interceptors.response.use(
   },
   async (error: AxiosError<ProblemDetail>) => {
     if (/Network Error/.test(error.message)) {
-      Toast.error("网络错误，请检查网络连接");
+      alert("网络错误，请检查网络连接");
       return Promise.reject(error);
     }
 
     const errorResponse = error.response;
 
     if (!errorResponse) {
-      Toast.error("网络错误，请检查网络连接");
+      alert("网络错误，请检查网络连接");
       return Promise.reject(error);
     }
 
     const { title, detail } = errorResponse.data;
 
     if (title && detail) {
-      Toast.error([title, detail].join(": "));
+      alert([title, detail].join(": "));
     }
 
     return Promise.reject(error);
